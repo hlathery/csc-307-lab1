054d8c9cd32d8290300cb462be679380c1576fd8
Hunter Lathery <hunter@Hunters-MacBook-Air-3.local>
2025-04-03 14:42:39 -0700

    Finished steps 5 & 6

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -6,6 +6,7 @@ function TableHead() {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
+                <th>Remove</th>
             </tr>
         </thead>
     );
@@ -17,6 +18,11 @@ function TableBody(props) {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => {props.removeCharacter(index)}}>
+                        Delete
+                    </button>
+                </td>
             </tr>
         );
     }
@@ -33,7 +39,10 @@ function Table(props) {
     return (
         <table>
             <TableHead />
-            <TableBody characterData={props.characterData}/>
+            <TableBody 
+                characterData={props.characterData}
+                removeCharacter={props.removeCharacter}
+            />
         </table>
     );
 }
